6849108903945f09598c561e82fd712b39a61b8b
AAdriane <adriane.albani2002@gmail.com>
2026-03-22 20:51:15 -0300

    feat: Criar chat e busca semântica

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,42 @@
+import os
 from search import search_prompt
+from dotenv import load_dotenv
+
+load_dotenv();
 
 def main():
-    chain = search_prompt()
 
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
-    
-    pass
+    print("Olá, escolha a opção e digite o número:");
+    print("1. Fazer uma pergunta sobre o contexto");
+    print("2. Sair");
+
+    choice = input();
+
+    while True:
+        match choice:
+            case "1":
+                print("-"*100);
+                question = input("Digite sua pergunta: ");
+
+                if not question:
+                    print("Você não digitou uma pergunta.");
+                    continue;
+
+                response = search_prompt(question);
+                
+                print(f"\nResposta: {response}");
+            
+                print("-"*100);
+            case "2":
+                print("Saindo do chat. Até mais!");
+                break;
+            case _:
+                print("Opção inválida. Por favor, escolha novamente.");
+
+        print("\nEscolha a opção e digite o número:");
+        print("1. Fazer uma pergunta");
+        print("2. Sair");
+        choice = input();
 
 if __name__ == "__main__":
     main()
--- src/search.py
+++ src/search.py
@@ -1,3 +1,18 @@
+import os;
+from dotenv import load_dotenv;
+from langchain_google_genai import GoogleGenerativeAIEmbeddings;
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI;
+from langchain_postgres import PGVector;
+from langchain.prompts import PromptTemplate;
+from langchain_core.output_parsers import StrOutputParser;
+
+load_dotenv();
+
+for k in ("GOOGLE_API_KEY", "DATABASE_URL","PG_VECTOR_COLLECTION_NAME", "PDF_PATH", "GOOGLE_EMBEDDING_MODEL", "GOOGLE_LLM_MODEL"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Variável de ambiente {k} não está definida");
+    
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -23,7 +38,39 @@ PERGUNTA DO USUÁRIO:
 {pergunta}
 
 RESPONDA A "PERGUNTA DO USUÁRIO"
-"""
+""";
 
 def search_prompt(question=None):
-    pass
+  if not question:
+      return ();
+
+  embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"));
+
+  store = PGVector(
+      embeddings=embeddings,
+      collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+      connection=os.getenv("DATABASE_URL"),
+      use_jsonb=True
+  );
+
+  result = store.similarity_search_with_score(question, k=10);
+
+  # concatenar o contexto
+  contexto = "\n".join([f"\n{doc.page_content}" for doc, _ in result]);
+
+  template = PromptTemplate(
+      template=PROMPT_TEMPLATE,
+      input_variables=["contexto", "pergunta"]
+  );
+
+  llm = ChatGoogleGenerativeAI(
+      model=os.getenv("GOOGLE_LLM_MODEL"),
+      temperature=0.5
+  );
+
+  chain = template | llm | StrOutputParser();
+
+  return chain.invoke({
+      "contexto": contexto,
+      "pergunta": question
+  });
