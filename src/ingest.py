fb8f993a28a83a2fbd9d10f7282b33a53cdca379
AAdriane <adriane.albani2002@gmail.com>
2026-03-22 20:50:51 -0300

    feat: Fazer ingestão do arquivo PDF no pgvector

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,56 @@
-import os
-from dotenv import load_dotenv
+import os;
+from dotenv import load_dotenv;
+from pathlib import Path;
+from langchain_community.document_loaders import PyPDFLoader;
+from langchain_text_splitters import RecursiveCharacterTextSplitter;
+from langchain_core.documents import Document;
+from langchain_postgres import PGVector;
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 
-PDF_PATH = os.getenv("PDF_PATH")
+for k in ("GOOGLE_API_KEY",  "DATABASE_URL", "PG_VECTOR_COLLECTION_NAME", "PDF_PATH", "GOOGLE_EMBEDDING_MODEL"):
+    if not os.getenv(k):
+        raise ValueError(f"Missing environment variable: {k}")
 
-def ingest_pdf():
-    pass
+# buscar documento
+current_dir = Path(__file__).parent;
+pdf_path = current_dir / os.getenv("PDF_PATH");
 
+# fazer chunck
+docs = PyPDFLoader(str(pdf_path)).load();
 
-if __name__ == "__main__":
-    ingest_pdf()
+splits = RecursiveCharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=150,
+    add_start_index=False
+    ).split_documents(docs);
+if not splits:
+    raise SystemExit(0);
+
+# enriquecer documentos
+enriched = [
+    Document(
+        page_content=d.page_content,
+        metadata={k: v for k, v in d.metadata.items() if v not in ("", None )}
+    )
+    for d in splits
+]
+
+ids = [f"doc-{i}" for i in range(len(enriched))];
+
+# criar embeddings
+embeddings = GoogleGenerativeAIEmbeddings(
+    model=os.getenv("GOOGLE_EMBEDDING_MODEL")
+);
+
+# criar vetor
+store = PGVector(
+    embeddings=embeddings,
+    collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+    connection=os.getenv("DATABASE_URL"),
+    use_jsonb=True
+);
+
+# adicionar documentos
+store.add_documents(documents=enriched, ids=ids);
